b978952da2234b9db3bf35a0e13ebdbd04dfb75f
Lukas Pitschl <lukas@dressyvagabonds.com>
2013-07-31 19:11:12 +0200

    Also unload if loaded via root.

--- Installer/libmacgpg-xpc-activate.sh
+++ Installer/libmacgpg-xpc-activate.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 sudo -u "$USER" launchctl remove org.gpgtools.Libmacgpg.jailfree 2>/dev/null
+sudo launchctl remove org.gpgtools.Libmacgpg.jailfree 2>/dev/null
 sudo -u "$USER" launchctl remove org.gpgtools.Libmacgpg.xpc 2>/dev/null
+sudo launchctl remove org.gpgtools.Libmacgpg.xpc 2>/dev/null
+
 rm -f "$HOME"/Library/LaunchAgents/org.gpgtools.Libmacgpg.xpc.plist
 sudo -u "$USER" launchctl load /Library/LaunchAgents/org.gpgtools.Libmacgpg.xpc.plist
 exit 0
