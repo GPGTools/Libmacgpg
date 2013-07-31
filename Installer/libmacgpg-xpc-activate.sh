33a2eef37a48b14f12f7e8a263c599c5faad2c8c
Mento <Mento@gpgtools.org>
2013-07-31 15:08:53 +0200

    Remove org.gpgtools.Libmacgpg.xpc.plist from self-compiled installations.

--- Installer/libmacgpg-xpc-activate.sh
+++ Installer/libmacgpg-xpc-activate.sh
@@ -2,5 +2,6 @@
 
 sudo -u "$USER" launchctl remove org.gpgtools.Libmacgpg.jailfree 2>/dev/null
 sudo -u "$USER" launchctl remove org.gpgtools.Libmacgpg.xpc 2>/dev/null
+rm -f "$HOME"/Library/LaunchAgents/org.gpgtools.Libmacgpg.xpc.plist
 sudo -u "$USER" launchctl load /Library/LaunchAgents/org.gpgtools.Libmacgpg.xpc.plist
 exit 0
