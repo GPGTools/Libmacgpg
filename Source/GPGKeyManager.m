f877338e210dc13151a20d169ae45595f340029b
Mento <Mento@gpgtools.org>
2015-12-11 15:39:08 +0100

    Only post a local notification.

--- Source/GPGKeyManager.m
+++ Source/GPGKeyManager.m
@@ -220,7 +220,7 @@ NSString * const GPGKeyManagerKeysDidChangeNotification = @"GPGKeyManagerKeysDid
 			userInfo = [NSDictionary dictionaryWithObject:affectedKeys forKey:@"affectedKeys"];
 		}
 		
-		[[NSDistributedNotificationCenter defaultCenter] postNotificationName:GPGKeyManagerKeysDidChangeNotification object:[[self class] description] userInfo:userInfo];
+		[[NSNotificationCenter defaultCenter] postNotificationName:GPGKeyManagerKeysDidChangeNotification object:[[self class] description] userInfo:userInfo];
 	});
 
 	// Start the key ring watcher.
