GPGTools/Libmacgpg

commit 050839d6bacdc9c3f853c833b2c797475eab95fe

Return explicit error codes, if decryption failed.

Source/GPGController.m
@@ -421,26 +421,33 @@ BOOL gpgConfigReaded = NO;
 		
 		__block BOOL failed = NO;
 		__block NSString *errorDecription = nil;
+		__block GPGErrorCode errorCode = 0;
 		NSArray *errorCodes = gpgTask.errorCodes;
 
 		// Check the error codes and some specific status codes to detect errors or possible attacks.
 		if (gpgTask.errorCode == GPGErrorCancelled) {
 			failed = YES;
+			errorCode = GPGErrorCancelled;
 			errorDecription = @"Decryption cancelled!";
-		} else if ([errorCodes containsObject:@(GPGErrorDecryptionFailed)]) {
-			failed = YES;
-			errorDecription = @"Decryption failed!";
 		} else if ([errorCodes containsObject:@(GPGErrorNoMDC)]) {
 			failed = YES;
+			errorCode = GPGErrorNoMDC;
 			errorDecription = @"Decryption failed: No MDC!";
 		} else if ([errorCodes containsObject:@(GPGErrorBadMDC)]) {
 			failed = YES;
+			errorCode = GPGErrorBadMDC;
 			errorDecription = @"Decryption failed: Bad MDC!";
+		} else if ([errorCodes containsObject:@(GPGErrorDecryptionFailed)]) {
+			failed = YES;
+			errorCode = GPGErrorDecryptionFailed;
+			errorDecription = @"Decryption failed!";
 		} else if (gpgTask.statusDict[@"NODATA"]) {
 			failed = YES;
+			errorCode = GPGErrorNoData;
 			errorDecription = @"Decryption failed: No Data!";
 		} else if (gpgTask.statusDict[@"FAILURE"]) {
 			failed = YES;
+			// Unknown error.
 			errorDecription = @"Decryption failed: Other Failure!";
 		} else {
 			// Check if there is an unencrypted plaintext in an encrypted message.
@@ -469,6 +476,7 @@ BOOL gpgConfigReaded = NO;
 			
 			if (hasUnencryptedPlaintext && hasDecryptedPacket) {
 				failed = YES;
+				errorCode = GPGErrorBadData;
 				errorDecription = @"Decryption failed: Unencrypted Plaintext!";
 			}
 		}
@@ -478,7 +486,7 @@ BOOL gpgConfigReaded = NO;
 				errorDecription = @"Decryption failed: Unknown Error!";
 			}
 			[output seekToBeginning];
-			@throw [GPGException exceptionWithReason:localizedLibmacgpgString(errorDecription) gpgTask:gpgTask];
+			@throw [GPGException exceptionWithReason:localizedLibmacgpgString(errorDecription) errorCode:errorCode gpgTask:gpgTask];
 		}
 	} @catch (NSException *e) {
 		[self handleException:e];

commit b2f99b3c8c962b83200b16f37bcae1de1380f8cd

Fix regression introduced with 050839d
Do not report DecryptionFailed if there is a more specific error code.
[#163 state:fixed assigned:mento]

Libmacgpg.xcodeproj/project.pbxproj
@@ -71,6 +71,7 @@
 		302481E01B5CF46800C86A07 /* Unarmor_ClearsignTwoLF.res in Resources */ = {isa = PBXBuildFile; fileRef = 302481D41B5CF46800C86A07 /* Unarmor_ClearsignTwoLF.res */; };
 		302481E11B5CF46800C86A07 /* Unarmor_ClearsignTwoLF.txt in Resources */ = {isa = PBXBuildFile; fileRef = 302481D51B5CF46800C86A07 /* Unarmor_ClearsignTwoLF.txt */; };
 		3026F4AF13A203A000F3CA02 /* Security.framework in Frameworks */ = {isa = PBXBuildFile; fileRef = 3026F4AE13A203A000F3CA02 /* Security.framework */; };
+		3044A92C20CA744000712FBD /* NoSecKey.gpg in Resources */ = {isa = PBXBuildFile; fileRef = 3044A92B20CA744000712FBD /* NoSecKey.gpg */; };
 		3048830B1462B11700F2E5F4 /* DirectoryWatcher.h in Headers */ = {isa = PBXBuildFile; fileRef = 304883091462B11700F2E5F4 /* DirectoryWatcher.h */; };
 		3048830C1462B11700F2E5F4 /* DirectoryWatcher.m in Sources */ = {isa = PBXBuildFile; fileRef = 3048830A1462B11700F2E5F4 /* DirectoryWatcher.m */; };
 		3048830F1462B22000F2E5F4 /* GPGWatcher.h in Headers */ = {isa = PBXBuildFile; fileRef = 3048830D1462B22000F2E5F4 /* GPGWatcher.h */; };
@@ -314,6 +315,7 @@
 		3026F4AE13A203A000F3CA02 /* Security.framework */ = {isa = PBXFileReference; lastKnownFileType = wrapper.framework; name = Security.framework; path = System/Library/Frameworks/Security.framework; sourceTree = SDKROOT; };
 		3034E5CA2065433E00E40A8E /* ja */ = {isa = PBXFileReference; lastKnownFileType = text.plist.strings; name = ja; path = ja.lproj/Localizable.strings; sourceTree = "<group>"; };
 		30444C331A65E0590052CB94 /* XCTest.framework */ = {isa = PBXFileReference; lastKnownFileType = wrapper.framework; name = XCTest.framework; path = Library/Frameworks/XCTest.framework; sourceTree = DEVELOPER_DIR; };
+		3044A92B20CA744000712FBD /* NoSecKey.gpg */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = text; path = NoSecKey.gpg; sourceTree = "<group>"; };
 		304883091462B11700F2E5F4 /* DirectoryWatcher.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = DirectoryWatcher.h; sourceTree = "<group>"; };
 		3048830A1462B11700F2E5F4 /* DirectoryWatcher.m */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.objc; path = DirectoryWatcher.m; sourceTree = "<group>"; };
 		3048830D1462B22000F2E5F4 /* GPGWatcher.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = GPGWatcher.h; sourceTree = "<group>"; };
@@ -888,6 +890,7 @@
 			children = (
 				30B11EDF20C91642006D333D /* NoMDC.gpg */,
 				30B7CE171EAE193F0050B9C5 /* Encrypted.gpg */,
+				3044A92B20CA744000712FBD /* NoSecKey.gpg */,
 				45B9A7F5151C9A6D00DAD67A /* OpenPGP.asc */,
 				45891A40151BA1EC002D7410 /* SignedInputStringCR.txt */,
 				45891A3C151BA1C9002D7410 /* SignedInputStringCRLF.txt */,
@@ -1137,6 +1140,7 @@
 				30A218731B5579A200D01E37 /* Unarmor_NoNewline.res in Resources */,
 				302481D61B5CF46800C86A07 /* Unarmor_ClearsignComplicated.clear in Resources */,
 				30ECA9591FBF4AE100C00279 /* Unarmor_UnicodeDashes.res in Resources */,
+				3044A92C20CA744000712FBD /* NoSecKey.gpg in Resources */,
 				30A218711B5579A200D01E37 /* Unarmor_MissingNewline.res in Resources */,
 				30B11EE120C91643006D333D /* NoMDC.gpg in Resources */,
 				304C69FF2069125000D89F65 /* Unarmor_2Messages.txt in Resources */,

Source/GPGController.m
@@ -441,7 +441,7 @@ BOOL gpgConfigReaded = NO;
 			errorDecription = @"Decryption failed: Bad MDC!";
 		} else if ([errorCodes containsObject:@(GPGErrorDecryptionFailed)]) {
 			failed = YES;
-			errorCode = GPGErrorDecryptionFailed;
+			errorCode = [errorCodes[0] intValue];
 			errorDecription = @"Decryption failed!";
 		} else if (gpgTask.statusDict[@"NODATA"]) {
 			failed = YES;

UnitTests/GPGControllerTest.m
@@ -87,5 +87,14 @@
 }
 
 
+- (void)testNoSecretKey {
+	NSData *encrypted = [GPGUnitTest dataForResource:@"NoSecKey.gpg"];
+
+	NSData *decrypted = [gpgc decryptData:encrypted];
+	
+	XCTAssertTrue(decrypted.length == 0, @"No secret key decryption did return some data!");
+	XCTAssertTrue([(GPGException *)gpgc.error errorCode] == GPGErrorNoSecretKey, @"Did not return a NoSecretKey error!");
+}
+
 
 @end

UnitTests/Resources/NoSecKey.gpg
@@ -1,0 +1,13 @@
+-----BEGIN PGP MESSAGE-----
+Comment: There is no secret key, to decrypt this message.
+
+hQEMA5lJlEyCBLrrAQgAsDbQ0yuek0rG40m4L18D6mpTC6nq0kKQXwybPgYgKq2Y
+e0h37Q2p2xZ0rGh+WsTP8O5YUJT/y1Zf5r7KQXh8HzFAR6I4npwftpH/w0+Jti6h
+gXaoi7MBujxWqON/nshj8BuhkC27Tb5Xsb6Vxzco4AWiZUJEyh5CwUZAdWuXOcVE
+a3XQ8C7AnJetskoKfow5eIVXr5Z8zx262Qm/x/JgCq8R17yQ95aLBFSwo3iTUGh6
+5AH2Py0VX/+dp5/TR7l4t1EZEUEO/x2Li4RISILZAA0LlzCjE+HrxAFBOTWsTFdZ
+KVfr0eOt7MZ+Z4Eyz82epxhmvjHf8aXNOjaD4CJmUdJPAfO+7eNOl4cGIc4yBNlV
+LgsVjiG5JOS4RtK68TW/7G5Kwy/qtPvYae2YbQSZXteULIfxM1tI29CHsC9V7RuL
+5p7bma/GFvLteuOY+i8aAw==
+=pYXj
+-----END PGP MESSAGE-----